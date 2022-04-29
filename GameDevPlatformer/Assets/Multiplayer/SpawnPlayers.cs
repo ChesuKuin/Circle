011ec5f50c2d4bd49a173ad2642f508efb586178
Dasha-coder659 <66951217+ChesuKuin@users.noreply.github.com>
2022-04-29 22:05:24 +0300

    Fixinf Bugs(?)

--- GameDevPlatformer/Assets/Multiplayer/SpawnPlayers.cs
+++ GameDevPlatformer/Assets/Multiplayer/SpawnPlayers.cs
@@ -5,6 +5,12 @@ using Photon.Pun;
 
 public class SpawnPlayers : MonoBehaviour
 {
+    [SerializeField] private CameraFollow _camera;
+    [SerializeField] private TeleportScript _teleport;
+    [SerializeField] private TeleportScript _teleport1;
+    [SerializeField] private TeleportScript _teleport2;
+    [SerializeField] private TeleportScript _teleport3;
+    [SerializeField] private TeleportScript _teleport4;
     public GameObject[] playerPrefabs;
     public Transform[] spawnPoints;
 
@@ -13,6 +19,16 @@ public class SpawnPlayers : MonoBehaviour
         int randomNumber = Random.Range(0, spawnPoints.Length);
         Transform spawnPoint = spawnPoints[randomNumber];
         GameObject playerToSpawn = playerPrefabs[(int)PhotonNetwork.LocalPlayer.CustomProperties["playerAvatar"]];
-        PhotonNetwork.Instantiate(playerToSpawn.name, spawnPoint.position, Quaternion.identity);
+        var go = PhotonNetwork.Instantiate(playerToSpawn.name, spawnPoint.position, Quaternion.identity);
+
+        if(go.GetComponent<PhotonView>().IsMine)
+        {
+            _teleport.player = go;
+            _teleport1.player = go;
+            _teleport2.player = go;
+            _teleport3.player = go;
+            _teleport4.player = go;
+            _camera.target = go.GetComponent<Controller2D>();
+        }
     }
 }
